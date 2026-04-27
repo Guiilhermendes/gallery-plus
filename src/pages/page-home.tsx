01d0d7d1d199b9cd52cb6fa32beb55735607d08b
Guilherme Mendes <guilhermemendeds@gmail.com>
2026-04-27 20:00:52 -0300

    feat: creating albums filter

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,11 @@
 import Container from "../components/container";
+import AlbumsFilter from "../context/albums/components/albums-filter";
 import PhotosList from "../context/photos/components/photos-list";
 
 export default function PageHome() {
     return (
         <Container>
+            <AlbumsFilter albums={[]} className="mb-9" />
             <PhotosList
                 photos={[
                     {
